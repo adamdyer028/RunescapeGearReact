e40316918a4ec7fea60c16469495088f2b60df68
Adam <adam.dyer028@gmail.com>
2023-08-29 13:36:30 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+
+function GearItem({imgSrc, className}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={className}>
+      <img src={imgSrc}></img>
     </div>
   );
 }
 
-export default App;
+function GearLayout() {
+  return (
+    <div className='gear-select'>
+      <div className='gear-items'>  
+        <GearItem className="head" imgSrc="/images/head.png"/>
+        
+        <GearItem className="cape" imgSrc="/images/cape.png"/>
+        <GearItem className="neck" imgSrc="/images/neck.png"/>
+        <GearItem className="ammo" imgSrc="/images/ammunition.png"/>
+        
+        <GearItem className="weapon" imgSrc="/images/weapon.png"/>
+        <GearItem className="body" imgSrc="/images/body.png"/>
+        <GearItem className="shield" imgSrc="/images/shield.png"/>
+        
+        <GearItem className="legs" imgSrc="/images/legs.png"/>
+
+        <GearItem className="hands" imgSrc="/images/hands.png"/>
+        <GearItem className="feet" imgSrc="/images/feet.png"/>
+        <GearItem className="ring" imgSrc="/images/ring.png"/>
+      </div>
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+  <div>
+    <h1>Select Your Gear:</h1>
+    <GearLayout /> 
+  </div>
+  );
+}
+
+
